c518efbce4e7262810777ac10a7585cc5606edd6
tasmia-rahman-maliha <maliha1288@gmail.com>
2022-09-29 16:32:35 +0600

    break time added

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Details.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLocationDot } from '@fortawesome/free-solid-svg-icons'
+import toast, { Toaster } from 'react-hot-toast';
 
 const Details = ({ selectedExercises }) => {
+    const [breakTime, setBreakTime] = useState(0);
+
+    let totalTime = 0;
+    for (const exercise of selectedExercises) {
+        totalTime = totalTime + exercise.time;
+    }
+    const notify = () => toast("Well done!");
+
+    const handleBreakTime = (time) => {
+        setBreakTime(time);
+    }
+    useEffect(() => {
+        localStorage.setItem('breakTime', breakTime);
+    }, [breakTime])
+
     return (
         <div className='details'>
             <div className='my-details'>
@@ -28,7 +44,25 @@ const Details = ({ selectedExercises }) => {
                     <h5>Age</h5>
                 </div>
             </div>
-            <h4>Add a break</h4>
+            <h4>Add A Break</h4>
+            <div className='breaks'>
+                <button onClick={() => handleBreakTime(10)}>10s</button>
+                <button onClick={() => handleBreakTime(20)}>20s</button>
+                <button onClick={() => handleBreakTime(30)}>30s</button>
+                <button onClick={() => handleBreakTime(40)}>40s</button>
+                <button onClick={() => handleBreakTime(50)}>50s</button>
+            </div>
+            <h4>Exercise Details</h4>
+            <div className='exercise-time'>
+                <p>Exercise Time</p>
+                <p>{totalTime} seconds</p>
+            </div>
+            <div className='break-time'>
+                <p>Break Time</p>
+                <p>{breakTime} seconds</p>
+            </div>
+            <button className='completed-btn' onClick={notify}>Activity Completed</button>
+            <Toaster />
         </div>
     );
 };
